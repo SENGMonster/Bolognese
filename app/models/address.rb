adf805ae8a5c3e710b7d6c0269eca3ec5311eef7
=MonaFHM <studium@ramonaleenings.com>
2012-11-09 14:53:04 +0100

    New Look

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,8 @@
 class Address < ActiveRecord::Base
   attr_accessible :city, :country, :description, :lat, :lng, :name, :plz, :street_nr, :web
   has_one :termine
+
+  def fulltext
+    return "#{street_nr}, #{city}, #{country}"
+  end
 end

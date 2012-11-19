90e2490f1e95819dddbde8685f084370a036004e
=MonaFHM <studium@ramonaleenings.com>
2012-11-19 08:53:33 +0100

    alles precomplied

---
+++ public/assets/tiny_mce/active_admin_custom-1c5799c584cfe16ce8810d81dd75d890.js
@@ -0,0 +1,1 @@
+tinyMCE.init({mode:"specific_textareas",editor_selector:"mceEditor",theme:"advanced",theme_advanced_toolbar_location:"top",theme_advanced_toolbar_align:"left",theme_advanced_resizing:!0});
